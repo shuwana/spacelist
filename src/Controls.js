bec846cb6708f378392f16a22071d2e8e06c60f4
Shuwana <shuwanab@icloud.com>
2023-04-02 22:22:06 -0400

    Changed the background color

--- src/Controls.js
+++ src/Controls.js
@@ -1,7 +1,9 @@
 const Controls = () => {
   return ( 
     <div className="flex-container">
-      <div className="tracks track1">1</div>
+      <div className="tracks track1">
+        
+      </div>
       <div className="tracks track2">2</div>
       <div className="tracks track3">3</div>
       <div className="tracks track4">4</div>
