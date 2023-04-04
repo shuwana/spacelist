99875bd3302a3e4ace80ab0394f365f1fc55e20b
Shuwana <shuwanab@icloud.com>
2023-04-03 22:27:39 -0400

    Created a placeholder list for the songs.

--- src/Controls.js
+++ src/Controls.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+
 const Controls = () => {
+  // Creating the states of tracks to display
+  const [tracks, setTracks] = useState([
+    {title: 'Song name', artist: 'Artist name', id: 1},
+    {title: 'Song name', artist: 'Artist name', id: 2},
+    {title: 'Song name', artist: 'Artist name', id: 3},
+    {title: 'Song name', artist: 'Artist name', id: 4},
+    {title: 'Song name', artist: 'Artist name', id: 5}
+  ]);
+
   return ( 
     <div className="flex-container">
-      <div className="tracks track1">
-        
+     {tracks.map((track) => (
+      // What is key={}?
+      <div className="flex-container">
+        <div className="track-preview" key={track.id}>
+        <h2>{ track.title }</h2>
+        <p>{ track.artist }</p>
       </div>
-      <div className="tracks track2">2</div>
-      <div className="tracks track3">3</div>
-      <div className="tracks track4">4</div>
-      <div className="tracks track5">5</div>
-    </div>
+      </div>
+      
+     ))}
+    </div> 
    );
 }
  
